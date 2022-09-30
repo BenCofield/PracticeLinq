4c275108a453709a092d3089f9d799b99043e3fd
Ben Cofield <benrcofield@gmail.com>
2022-09-30 11:52:25 -0400

    first commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Transactions;
+
+List<string> GamesList = new List<string>()
+{
+    "Super Mario Kart",
+    "Mario Kart 64",
+    "Mario Kart: Super Circuit",
+    "Mario Kart: Double Dash",
+    "Mario Kart Arcade",
+    "Mario Kart DS",
+    "Mario Kart Wii",
+    "Mario Kart 7",
+    "Mario Kart 8"
+};
+
+GamesList.OrderBy(x => x.Length)
+         .ThenBy(x => x)
+         .ToList()
+         .ForEach(x => Console.WriteLine(x));
